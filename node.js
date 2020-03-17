369327466227dbb7f19bf94394ad3591cb8a0c47
Nícholas Oliveira <nicholas.andre@10up.com>
2020-03-17 12:24:41 -0300

    Moving lib/common to index.js and adding more rules for node.js

--- node.js
+++ node.js
@@ -4,4 +4,10 @@ module.exports = {
 		es6: true,
 	},
 	extends: './index',
+	rules: {
+		'no-path-concat': ['error'],
+		'no-process-exit': ['error'],
+		'global-require': ['error'],
+		'no-new-require': ['error'],
+	},
 };
