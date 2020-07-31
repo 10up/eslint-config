4496938ea26baa6e5e4bfcb80118c20b0a362f07
Ivan Kristianto <ivan.kristianto@gmail.com>
2020-07-31 21:57:20 +0700

    Update tests for react rules

---
+++ fixtures/react/pass/jsx-filename-extension.js
@@ -0,0 +1,10 @@
+/* eslint-disable no-unused-vars */
+/* eslint-disable no-unused-expressions */
+/* eslint-disable import/no-unresolved */
+/* eslint-disable react/react-in-jsx-scope */
+
+const testFunc = () => {
+	// The rule will allow .js extension.
+};
+
+<testFunc />;
